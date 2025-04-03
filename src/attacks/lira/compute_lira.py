72e7cb4a1a14b9122bd831b24032b92143de0eab
Jebastin Nadar <jebastin.nadar24@imperial.ac.uk>
2025-04-03 10:37:11 +0530

    Initialize lira with code from Xavier's repo

---
+++ src/attacks/lira/compute_lira.py
@@ -0,0 +1,210 @@
+import argparse
+import pickle
+from collections import defaultdict
+from os import cpu_count
+from pathlib import Path
+from typing import Dict, List, Optional, Tuple
+
+import numpy as np
+from joblib import Parallel, delayed  # type: ignore
+from numpy.typing import NDArray as Array
+from scipy.stats import norm  # type: ignore
+from tqdm import tqdm  # type: ignore
+
+from munl.datasets import get_dataset_and_lengths
+from munl.datasets.cifar10 import get_cifar10_test_transform
+
+
+def predicted_membership_probability(
+    z, forget_mean, forget_sigma, never_mean, never_sigma
+):
+    numerator = norm.pdf(z, forget_mean, forget_sigma)
+    denominator = norm.pdf(z, forget_mean, forget_sigma) + norm.pdf(
+        z, never_mean, never_sigma
+    )
+    return numerator / denominator
+
+
+def compute_membership_probabilities(id_to_correct_probas):
+    ndx_to_membership = defaultdict(list)
+
+    # For every sample ndx in the dataset, we have computed the
+    # confidence of the correct label
+    for ndx in id_to_correct_probas:
+        # Obtain the mean (mu1) and standard deviation (sigma1) for
+        # the 'forgotten' vector (A)
+        forget_mean = np.mean(id_to_correct_probas[ndx].forgotten)
+        forget_std = np.std(id_to_correct_probas[ndx].forgotten)
+
+        # Obtain the mean (mu0) and standard deviation (sigma0) for the
+        # 'never' vector (B)
+        never_mean = np.mean(id_to_correct_probas[ndx].never)
+        never_std = np.std(id_to_correct_probas[ndx].never)
+
+        # For each predicted probability in the 'forgotten' vector (A),
+        # compute the membership probability
+        # for proba in id_to_correct_probas[ndx].forgotten:
+        #     membership_prob = predicted_membership_probability(proba,
+        # forget_mean, forget_std, never_mean, never_std)
+        #     ndx_to_membership[ndx].append(membership_prob)
+        ndx_to_membership[ndx] = Parallel(n_jobs=max(cpu_count() - 1, 1))(
+            delayed(predicted_membership_probability)(
+                proba, forget_mean, forget_std, never_mean, never_std
+            )
+            for proba in id_to_correct_probas[ndx].forgotten
+        )
+
+    return ndx_to_membership
+
+
+class NeverAndForgotten:
+    def __init__(
+        self,
+        never: Optional[List[Tuple[int, int]]] = None,
+        forgotten: Optional[List[Tuple[int, int]]] = None,
+    ):
+        self.never = never if never is not None else []
+        self.forgotten = forgotten if forgotten is not None else []
+
+    def assert_no_overlap(self):
+        unique_never = set(self.never)
+        unique_forgotten = set(self.forgotten)
+        assert len(unique_never.intersection(unique_forgotten)) == 0
+
+    def __repr__(self):
+        rep = f"Never {len(self.never)} {self.never[:10]}, Forgotten "
+        rep += f"{len(self.forgotten)} {self.forgotten[:10]}"
+        return rep
+
+
+class ProbasNeverAndForgotten:
+    def __init__(
+        self,
+        never: Optional[List[float]] = None,
+        forgotten: Optional[List[float]] = None,
+    ):
+        self.never = never if never is not None else []
+        self.forgotten = forgotten if forgotten is not None else []
+
+    def __repr__(self):
+        rep = f"Probas Never {np.mean(self.never):.3f}+-{np.std(self.never):.3f} "
+        rep += f"Forgotten  {np.mean(self.forgotten):.3f}+-{np.std(self.forgotten):.3f}"
+        return rep
+
+
+def extract_correct_probabilities(
+    probas: Array, targets: Array, indices: Dict[int, NeverAndForgotten]
+):
+    print(probas.shape)
+    indices_to_correct_probas_never_and_forgotten = {}
+    for ndx in sorted(indices):
+        indices_to_correct_probas_never_and_forgotten[ndx] = ProbasNeverAndForgotten()
+        forgotten = indices[ndx].forgotten
+        for split_ndx, forget_ndx in forgotten:
+            proba = probas[ndx, split_ndx, forget_ndx]
+            correct_proba = proba[targets[ndx]]
+            indices_to_correct_probas_never_and_forgotten[ndx].forgotten.append(
+                correct_proba
+            )
+        for split_ndx, forget_ndx in indices[ndx].never:
+            proba = probas[ndx, split_ndx, forget_ndx]
+            correct_proba = proba[targets[ndx]]
+            indices_to_correct_probas_never_and_forgotten[ndx].never.append(
+                correct_proba
+            )
+    return indices_to_correct_probas_never_and_forgotten
+
+
+def reconstruct_split_and_forget(
+    lira_root: Path,
+    num_splits: int = 64,
+    num_forgets: int = 10,
+    num_elements: int = 2375,
+):
+    reconstructed = np.zeros((num_splits, num_forgets, num_elements), dtype=int)
+    for split_ndx in range(num_splits):
+        forgets = lira_root / str(split_ndx) / "forgets.npy"
+        data = np.load(forgets)
+        reconstructed[split_ndx] = data
+    return reconstructed
+
+
+def main(args):
+    print("Computing final LIRAs")
+    unlearners = args.unlearners
+    lira_root = args.lira_root
+    lira_preds = args.lira_preds
+    args.test_indices = np.load(args.lira_root / "test_matrices.npy")
+
+    cifar_complete, _ = get_dataset_and_lengths(
+        Path("datasets"), "cifar10", transform=get_cifar10_test_transform()
+    )
+    targets = np.concatenate([cifar_complete.datasets[ndx].targets for ndx in range(2)])
+
+    forgets_splits_and_forget_indices = reconstruct_split_and_forget(lira_root)
+
+    print("Populating id_to_forgotten_never_seen")
+    id_to_forgotten_never_seen = {}
+    # Basically we go through the test indices split_indices
+    for split_ndx, row in enumerate(TEST_INDICES):
+        for value in row:
+            if value not in id_to_forgotten_never_seen:
+                id_to_forgotten_never_seen[value] = NeverAndForgotten()
+            id_to_forgotten_never_seen[value].never.extend(
+                [(split_ndx, forget_ndx) for forget_ndx in range(10)]
+            )
+    for split_ndx in range(64):
+        for forget_ndx in range(10):
+            for value in forgets_splits_and_forget_indices[split_ndx, forget_ndx]:
+                id_to_forgotten_never_seen[value].forgotten.append(
+                    (split_ndx, forget_ndx)
+                )
+
+    print("Populating id_to_correct_probas")
+    for unlearner in tqdm(unlearners):
+        storage = get_preds(lira_preds, unlearner)
+        id_to_correct_probas = extract_correct_probabilities(
+            storage, targets, id_to_forgotten_never_seen
+        )
+        ndx_to_membership = compute_membership_probabilities(id_to_correct_probas)
+        formatable_data_path = "lira/{unlearner}_membership.npy"
+        data_path = formatable_data_path.format(unlearner=unlearner)
+        with open(data_path, "wb") as out_fo:
+            pickle.dump(obj=ndx_to_membership, file=out_fo)
+        del storage
+
+
+def get_preds(lira_preds: Path, unlearner: str):
+    storage = np.zeros(shape=(64, 10, 60_000, 10))
+    print(
+        storage.itemsize * storage.size,
+    )
+    unlearner_dir = lira_preds / unlearner
+    ndx = 0
+    for split_ndx in range(64):
+        for forget_ndx in range(10):
+            model = f"resnet18_0_{split_ndx}_{forget_ndx}.npy"
+            preds = np.load(unlearner_dir / model)
+            assert preds.shape == (60_000, 10)
+            storage[split_ndx][forget_ndx] = preds
+            ndx += 1
+    res = np.transpose(storage, (2, 0, 1, 3))
+    return res
+
+
+if __name__ == "__main__":
+    # LIRA_ROOT = Path("artifacts") / "lira"
+
+    # TRAIN_INDICES = np.load(LIRA_ROOT / "train_matrices.npy")
+    # TEST_INDICES = np.load(LIRA_ROOT / "test_matrices.npy")
+    args = argparse.Namespace()
+    args.lira_root = Path(
+        "/home/gcp-compute/for_reproducibility/lira_predictions/lira"
+    )
+    args.lira_preds = Path("lira") / "predictions"
+    # args.unlearners = ["naive", "original", "kgltop2", "kgltop3", "kgltop4",
+    # "kgltop5", "kgltop6"]
+    # args.unlearners = ["naive", "original", "kgltop2", "kgltop3", "kgltop5", "kgltop6"]
+    # args.unlearners = ["finetune", "kgltop4"]
+    args.unlearners = ["kgltop4"]
+    main(args)
