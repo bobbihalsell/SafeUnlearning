bcc11b061861455dcc99ebc402edf8fb662fa0d7
Bobbi <blh124@ic.ac.uk>
2025-04-11 09:58:48 +0100

    feature tested

--- src/datasets/runs/test_run.sh
+++ src/datasets/runs/test_run.sh
@@ -1,1 +1,6 @@
-python src/datasets/main.py forget=instance dataset=cifar10 forget.forget_idx="[216, 32245, 27206, 10863, 2190, 31849, 25408, 33504]" forget.retain_size=15 dataset.init_dir=./raw dataset.save_dir=./data
+# instance
+# python src/datasets/main.py forget=instance dataset=cifar10 forget.forget_idx="[216, 32245, 27206, 10863, 2190, 31849, 25408, 33504]" forget.retain_size=15 dataset.init_dir=./raw dataset.save_dir=./data
+# class
+# python src/datasets/main.py forget=class dataset=cifar10 forget.forget_idx="[5]" dataset.init_dir=./raw dataset.save_dir=./data
+# class num
+python src/datasets/main.py forget=classnum dataset=cifar10 forget.forget_idx="{1:10, 5:10}" dataset.init_dir=./raw dataset.save_dir=./data
