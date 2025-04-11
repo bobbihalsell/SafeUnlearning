1c3f27af01fd7cd6fd2d5d68a46a216677846fc1
Jebastin Nadar <jebastin.nadar24@imperial.ac.uk>
2025-04-11 15:29:38 +0530

    changes to lira trainer

--- src/attacks/lira/train_lira.py
+++ src/attacks/lira/train_lira.py
@@ -1,48 +1,62 @@
-from utils import get_retain_forget_val_test_indices
-from munl.datasets import get_loaders_from_dataset_and_unlearner_from_cfg_with_indices
+import os
+import torch
+from utils import get_retain_forget_val_indices, get_loaders_from_indices
 from pathlib import Path
 import numpy as np
-
 from src.unlearning.main import UnlearnApp
 
 
-def main(args, dataset_cfg, model_cfg, unlearner_cfg):
-    unlearner_name = args.unlearner_name
-    model_name = args.model
-    dataset_name = args.dataset
-    root = args.root
-    seed = args.seed
-    num_splits = args.num_splits
-    num_forgets = args.num_forgets
+class UnlearnAppForLiRA(UnlearnApp):
+    def __init__(self, config):
+        super().__init__(config)
 
-    name_save_path = "artifacts/lira/unlearn"
+    def run(self, dataloaders):
+        print('running...')
+        # Step 1: Initialize the model
+        original_model = self.load_model_from_disk(self.model_ckpt_path)
+
+        # Step 2: Unlearning
+        unlearner = self.initialize_unlearner()
+        print('unlearner initialized')
+        unlearned_model, losses = unlearner.unlearn(original_model,
+                                                    data_dict=dataloaders,
+                                                    verbose=self.verbose,
+                                                    **self.unlearn_params)
+        print('model unlearned')
+
+        # Step 3: Save the unlearned model
+        directory = f'{self.output_dir}/unlearn/{self.unlearning_algorithm}'
+        os.makedirs(directory, exist_ok=True)  # Ensure the directory exists
+
+        filepath = os.path.join(directory, f'{self.model_name}_{self.seed}_{self.model_type}.pt')
+        torch.save(unlearned_model.state_dict(), filepath)
+
+
+def main(config, unlearner):
+    model_name = config.model.name
+    dataset_name = config.dataset.name
+    root = config.root
+    seed = config.seed
+    num_splits = config.dataset.num_splits
+    num_forgets = config.dataset.num_forgets
+
+    name_save_path = root + "unlearn"
     if not Path(name_save_path).exists():
         Path(name_save_path).mkdir(parents=True, exist_ok=True)
 
+    app = UnlearnAppForLiRA(config)
+
     for split_ndx in range(num_splits):
         for forget_ndx in range(num_forgets):
-            retain, forget, val, test = get_retain_forget_val_test_indices(
-                lira_path=root / "artifacts" / "lira" / "splits",
+            retain, forget, val = get_retain_forget_val_indices(
+                lira_path=root / "splits",
                 split_ndx=split_ndx,
                 forget_ndx=forget_ndx,
             )
 
-            train_loader, retain_loader, forget_loader, val_loader, test_loader = (
-                get_loaders_from_dataset_and_unlearner_from_cfg_with_indices(
-                    root=root,
-                    indices=[np.concatenate([retain, forget]), retain, forget, val, test],
-                    dataset_cfg=dataset_cfg,
-                    unlearner_cfg=unlearner.cfg,
-                )
-            )
-
-            model = app.get_model(root / "artifacts" / dataset_name)
-            original_model, unlearned_model = app.run_from_model_and_loaders(
+            retain_loader, forget_loader, val_loader = get_loaders_from_indices(
                 root=root,
-                model=model,
-                loaders=(train_loader, retain_loader, forget_loader, val_loader, test_loader),
-                save=True,
-                save_path=name_save_path,
-                save_name=f"{split_ndx}_{forget_ndx}",
-                unlearner_name=unlearner_name,
+                indices=[retain, forget, val],
+                config=config,
             )
+            app.run([retain_loader, forget_loader, val_loader])
--- src/attacks/lira/utils.py
+++ src/attacks/lira/utils.py
@@ -1,25 +1,58 @@
 from pathlib import Path
 import numpy as np
+from typing import List, Dict
+from torch.utils.data import DataLoader
 
 
-def get_retain_forget_val_test_indices(
+def get_retain_forget_val_indices(
     lira_path: Path, split_ndx: int, forget_ndx: int
 ):
     retains = np.load(lira_path / str(split_ndx) / "retains.npy")
     forgets = np.load(lira_path / str(split_ndx) / "forgets.npy")
     vals = np.load(lira_path / "val_matrices.npy")
-    tests = np.load(lira_path / "test_matrices.npy")
 
     retain_indices = retains[forget_ndx]
     forget_indices = forgets[forget_ndx]
     val_indices = vals
-    test_indices = tests[split_ndx]
 
     assert set(retain_indices) & set(forget_indices) == set()
-    assert set(retain_indices) & set(test_indices) == set()
-    assert set(forget_indices) & set(test_indices) == set()
-    assert set(val_indices) & set(test_indices) == set()
     assert set(retain_indices) & set(val_indices) == set()
     assert set(forget_indices) & set(val_indices) == set()
-    assert set(test_indices) & set(val_indices) == set()
-    return retain_indices, forget_indices, val_indices, test_indices
+    return retain_indices, forget_indices, val_indices
+
+
+def get_loaders_from_indices(
+    root: Path,
+    indices: List,
+    config: Dict,
+) -> List[DataLoader]:
+    dataset_root = root / "datasets"
+    dataset_name = dataset_cfg.name
+    non_augmented_dataset, _ = get_dataset_and_lengths(
+        dataset_root, dataset_name, get_train_transform(dataset_name)
+    )
+    augmented_dataset, _ = get_dataset_and_lengths(
+        dataset_root, dataset_name, get_test_transform(dataset_name)
+    )
+
+    retain_indices, forget_indices, val_indices = indices
+    loaders = []
+    for split_name, indices in zip(["retain", "forget", "val"], [retain_indices, forget_indices, val_indices]):
+        data_set = get_dataset_based_on_split_state(
+            non_augmented_dataset,
+            augmented_dataset,
+            getattr(unlearner_cfg.loaders, split_name).state,
+            indices,
+        )
+        loader = DataLoader(
+            data_set,
+            batch_size=unlearner_cfg.batch_size,
+            shuffle=getattr(unlearner_cfg.loaders, split_name).shuffle,
+            num_workers=get_num_workers_from_shuffle(
+                getattr(unlearner_cfg.loaders, split_name).shuffle
+            ),
+            pin_memory=False,
+        )
+        loaders.append(loader)
+
+    return loaders
