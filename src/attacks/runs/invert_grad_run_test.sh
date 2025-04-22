8fe29d85ba4d89ec38f7fde420229d60623bfdb4
Varsha Bubathi <varsha.bubathi24@imperial.ac.uk>
2025-04-22 21:43:54 +0100

    test run ivnertgrad

--- src/attacks/runs/invert_grad_run_test.sh
+++ src/attacks/runs/invert_grad_run_test.sh
@@ -1,6 +1,6 @@
 python src/attacks/main_reconstructor.py dataset=cifar10 reconstructor=invertgrad model=torchvision +wandb=reconstruction \
     dataset.save_path=./data \
-    reconstructor.unlearned_labels=[0] \
+    reconstructor.unlearned_labels=[0] reconstructor.cfg.recon_iterations=500 \
     model.original_model_ckpt_path=/vol/bitbucket/vb524/v2_safe/safe-unlearning/model/neggrad_1s_1e/unlearn/neggrad/resnet18_42_unlearned.pt \
     model.unlearned_model_ckpt_path=/vol/bitbucket/vb524/v2_safe/safe-unlearning/model/neggrad_1s_1e/unlearn/neggrad/resnet18_42_original.pt \
     model.model_name=resnet18 \
