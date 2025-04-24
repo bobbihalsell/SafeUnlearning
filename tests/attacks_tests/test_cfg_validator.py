7f5f62f888f28b9155ffaeee952850604594d72b
Varsha Bubathi <varsha.bubathi24@imperial.ac.uk>
2025-04-24 16:01:43 +0100

    move around imports

--- tests/attacks_tests/test_cfg_validator.py
+++ tests/attacks_tests/test_cfg_validator.py
@@ -1,12 +1,11 @@
 import os
 import unittest
 import tempfile
-from attacks.config_validation import ReconstructorValidator
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../src')))
 
 
-
+from attacks.config_validation import ReconstructorValidator
 from cfg_validator import ConfigError
 
 class TestGGLValidation(unittest.TestCase):
