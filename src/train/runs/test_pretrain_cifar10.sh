f2aafda1b8c01eabc760ed8f7c0b7eccd966e836
Andrew Siow <andrewsiow@Andrews-MacBook-Pro.local>
2025-04-25 16:32:25 +0100

    Modified pretrain CIFAR script

--- src/train/runs/test_pretrain_cifar10.sh
+++ src/train/runs/test_pretrain_cifar10.sh
@@ -1,1 +1,1 @@
-python src/train/main.py dataset=cifar10 model=torchvision model.model_name=resnet18 trainer=trainer dataset.save_path=./data model.pretrained=true trainer.model_save_dir=./artifacts/models/cifar10 trainer.cfg.epochs=3 experiment_name=test +wandb=default
+python src/train/main.py dataset=cifar10 model=torchvision model.model_name=resnet18 trainer=trainer dataset.save_path=./data model.pretrained=true trainer.model_save_dir=./artifacts/models/cifar10 trainer.cfg.epochs=3 trainer.cfg.lr=0.0005 experiment_name=test +wandb=default
